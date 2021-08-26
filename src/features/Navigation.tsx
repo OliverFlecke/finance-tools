2ddbb85c30fd873df62ca201501fb5e766d5c0b6
Oliver Fleckenstein <oliverfl@live.dk>
2021-08-26 21:01:25 +0200

    Navigation for larger screens as well

--- src/features/Navigation.tsx
+++ src/features/Navigation.tsx
@@ -1,32 +1,42 @@
-import React from 'react';
-import { useState } from 'react';
-import { IoMenuOutline } from 'react-icons/io5';
+import React, { useState } from 'react';
+import { IoCloseOutline, IoMenuOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
+
+const links = [
+	{ path: '/', text: 'Overview' },
+	{
+		path: '/stocks',
+		text: 'Stocks',
+	},
+	{ path: '/compound-calculator', text: 'Compound calculator' },
+];
 
 const Navigation: React.FC = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<nav>
+		<nav className="flex flex-col md:flex-row">
 			<div className="flex items-center">
-				<button onClick={() => setIsOpen((x) => !x)}>
-					<IoMenuOutline size={32} />
+				<button className="md:hidden" onClick={() => setIsOpen((x) => !x)}>
+					{isOpen ? <IoCloseOutline size={32} /> : <IoMenuOutline size={32} />}
 				</button>
 				<Link to="/">
-					<h1 className="px-4 text-xl uppercase font-sans font-light">Finance tracker</h1>
+					<h1 className="px-4 md:px-0 text-xl uppercase font-sans font-light">Finance tracker</h1>
 				</Link>
 			</div>
 
-			<ul className={`${isOpen ? 'flex' : 'hidden'} flex-col`}>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/stocks">Stocks</Link>
-				</li>
-				<li>
-					<Link to="/compound-calculator">Compound calculator</Link>
-				</li>
+			<ul
+				className={`${
+					isOpen ? 'flex' : 'hidden'
+				} flex-col md:flex md:flex-row md:items-center md:space-x-4 md:pl-8`}
+			>
+				{links.map((x) => (
+					<li key={x.path}>
+						<Link to={x.path} className="align-middle h-full font-light hover:underline">
+							{x.text}
+						</Link>
+					</li>
+				))}
 			</ul>
 		</nav>
 	);
--- src/features/login/LoginState.tsx
+++ src/features/login/LoginState.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
+import { IoLogOutOutline } from 'react-icons/io5';
 import { User } from 'utils/githubAuth';
 
 interface LoginStateProps {
@@ -8,6 +10,8 @@ interface LoginStateProps {
 }
 
 const LoginState: React.FC<LoginStateProps> = ({ user, authorizeUrl, logout }: LoginStateProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+
 	if (user === null) {
 		return (
 			<a className="btn btn-primary" href={authorizeUrl}>
@@ -19,19 +23,26 @@ const LoginState: React.FC<LoginStateProps> = ({ user, authorizeUrl, logout }: L
 	return (
 		<div className="flex items-center space-x-4">
 			<span className="hidden sm:inline">{user.login}</span>
-			<div className="group">
-				<img
-					src={user.avatar_url}
-					alt="Avatar of the logged in user"
-					className="max-h-10 rounded-full"
-					loading="lazy"
-				/>
-				<div className="hidden group-hover:block absolute rounded py-4 shadow bg-coolGray-100 dark:bg-coolGray-700">
+			<div className="group" onMouseLeave={() => setIsOpen(false)}>
+				<button onClick={() => setIsOpen((x) => !x)}>
+					<img
+						src={user.avatar_url}
+						alt="Avatar of the logged in user"
+						className="max-h-10 rounded-full"
+						loading="lazy"
+					/>
+				</button>
+				<div
+					className={`${
+						isOpen ? '' : 'hidden'
+					} group-hover:block absolute right-0 rounded py-4 shadow bg-coolGray-100 dark:bg-coolGray-700`}
+				>
 					<button
 						onClick={logout}
-						className="btn hover:text-coolGray-900 dark:hover:text-coolGray-400 hover:underline"
+						className="btn flex items-center space-x-2 hover:text-coolGray-900 dark:hover:text-coolGray-400 hover:underline"
 					>
-						Logout
+						<IoLogOutOutline className="inline" />
+						<span className="align-middle">Logout</span>
 					</button>
 				</div>
 			</div>
