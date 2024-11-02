986da855b94bc21f63f6d8ebaca5f7f2f7bab4c1
Oliver Fleckenstein <oliverfl@live.dk>
2024-11-02 18:14:22 +0800

    chore: migrated currency API

--- src/features/Currency/api.ts
+++ src/features/Currency/api.ts
@@ -10,5 +10,5 @@ export interface CurrencyRates {
 }
 
 function getUrl(currency: string) {
-	return `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.min.json`;
+	return `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.min.json`;
 }
