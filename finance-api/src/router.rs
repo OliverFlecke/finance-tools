7a1cd9c52e112f01fec0e67e79673b17a7032c61
Oliver Fleckenstein <oliverfl@live.dk>
2026-07-01 11:53:40 +0200

    chore: update config

--- finance-api/src/router.rs
+++ finance-api/src/router.rs
@@ -6,9 +6,11 @@ use crate::{account::account_router, health::health_check_router, state::AppStat
 
 pub fn build_router(state: AppState) -> Router {
 	// TODO: CORS should not be necessary when running in production.
-	let cors = CorsLayer::very_permissive()
-		.allow_origin(HeaderValue::from_static("https://localhost:3000"))
-		.allow_origin(HeaderValue::from_static("https://finance.oliverflecke.me"));
+	let cors = CorsLayer::very_permissive().allow_origin([
+		HeaderValue::from_static("https://localhost:3000"),
+		HeaderValue::from_static("https://finance.oliverflecke.me"),
+		HeaderValue::from_static("https://finance-tools.oliverfl.workers.dev"),
+	]);
 
 	Router::new()
 		.nest("/api/v1/account", account_router())
